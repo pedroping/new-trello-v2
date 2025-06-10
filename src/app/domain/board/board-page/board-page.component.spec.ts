e29bbf0b2337a1aa8da2c9d959c0326d62ba91cd
pedroping <pedro.dias@moneyprestador.com.br>
2025-06-10 18:28:22 -0300

    Feat: start skeleton page refactor to reduce scss bundle size

--- src/app/domain/board/board-page/board-page.component.spec.ts
+++ src/app/domain/board/board-page/board-page.component.spec.ts
@@ -1,20 +1,15 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BoardPageComponent } from './board-page.component';
 
 describe('BoardPageComponent', () => {
   let component: BoardPageComponent;
   let fixture: ComponentFixture<BoardPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ BoardPageComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [BoardPageComponent],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BoardPageComponent);
