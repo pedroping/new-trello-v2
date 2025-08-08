b7c7acb258bcea4c45841724ca4b174649302b51
pedroping <pedro.dias@moneyprestador.com.br>
2025-08-08 09:35:44 -0300

    Fix: manage version updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'new-trello-root',
   template: '<router-outlet />',
   imports: [RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly swUpdate = inject(SwUpdate);
+
+  constructor() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event) => {
+        if (event.type === 'VERSION_READY') {
+          this.activateUpdate();
+        }
+      });
+    }
+  }
+
+  private activateUpdate() {
+    this.swUpdate.activateUpdate().then(() => {
+      document.location.reload();
+    });
+  }
+}
